355a50612f129ca6edba547e9149b4b2fcd151b2
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-08-12 23:34:49 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,108 +1,104 @@
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import { AppBar, Box, IconButton } from "@mui/material";
-import { useState } from "react";
-import { Link as Mlink } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import navImage from "../assets/12th Project/Logo Files/PNGs & SVGs/1x/Asset 3.png";
-const Navbar = () => {
-  const [state, setState] = useState(false);
+import navNewImg from "../assets/12th Project/Logo Files/PNGs & SVGs/1x/Asset 2.png";
+import "../Navbar.css";
+import co2 from "../assets/orange-co2-icon.avif";
+import forest from "../assets/green-forest-icon.avif";
+// import aboutIcon from "../assets/blue-icon.avif";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = ({ params }) => {
+  const [navShow, setNavShow] = useState(false);
+  const [newImg, setNewImg] = useState();
+  const navigate = useNavigate();
+
   return (
-    <AppBar position='sticky'>
-      <Box
-        bgcolor='white'
-        display='flex'
-        justifyContent={"space-between"}
-        paddingX={{ xs: "1.25rem", md: "10rem" }}
-        paddingY={"1.25rem"}
-        alignItems={"center"}
-      >
-        <Box>
-          <Mlink
-            target='_blank'
-            rel='noopener'
-            href='https://for-zero-web.vercel.app/'
-          >
-            <Box width={{ xs: "4.9rem" }} className='logoimg'>
-              {/* 4.9rem */}
-              <img
-                style={{ width: "100%" }}
-                src={navImage}
-                alt='lowsootoisdfj'
-              />
-            </Box>
-          </Mlink>
-        </Box>
-        <div className='c_links'>
-          <Box display='flex' gap={"0.5rem"}>
-            <Mlink
-              underline='none'
-              fontSize='1.125rem'
-              target='_blank'
-              rel='noopener'
-              sx={{ color: "black" }}
-              href='https://for-zero-web.vercel.app/our-projetcs'
-            >
-              Our Projects
-            </Mlink>
-            <Mlink
-              underline='none'
-              fontSize='1.125rem'
-              target='_blank'
-              rel='noopener'
-              sx={{ color: "black" }}
-              href='https://for-zero-web.vercel.app/our-approach'
-            >
-              Our Approach
-            </Mlink>
-          </Box>
+    <div className='navbar-full' id='navbar-full'>
+      <nav className={`navbar bl-nav `} id='navbar'>
+        <div className='img-nav'>
+          <img src={navNewImg} alt='' />
         </div>
-        <div className='c_navtoggle'>
-          <Box
-            onClick={() => setState((curr) => !curr)}
-            display='flex'
-            gap={"0.5rem"}
-          >
-            {/* sx={{ color: "primary" }} */}A
-            <IconButton color='primary'>
-              {state ? <CloseIcon /> : <MenuIcon />}
-            </IconButton>
-          </Box>
-        </div>
-      </Box>
-      {state && (
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          paddingX={"2rem"}
-          width={"100%"}
-          gap={"0.5rem"}
-          paddingBottom={"1rem"}
-          justifyContent={"center"}
-          bgcolor='white'
-        >
-          <Mlink
-            underline='none'
-            fontSize='1.125rem'
-            target='_blank'
-            rel='noopener'
-            sx={{ color: "black" }}
-            href='https://for-zero-web.vercel.app/our-projects'
-          >
+        <ul className='nav-items'>
+          <li className='item' onClick={() => navigate("/our-projects")}>
             Our Projects
-          </Mlink>
-          <Mlink
-            underline='none'
-            fontSize='1.125rem'
-            target='_blank'
-            rel='noopener'
-            sx={{ color: "black" }}
-            href='https://for-zero-web.vercel.app/our-approach'
-          >
+          </li>
+          <li className='item' onClick={() => navigate("/our-approach")}>
             Our Approach
-          </Mlink>
-        </Box>
+          </li>
+          {/* <li className='item'>Contact Us</li> */}
+          <li className='btn-item item'>
+            <a href='https://dancing-frangipane-786e1e.netlify.app/'>
+              <button className='btn nav-btn'>Get Started</button>
+            </a>
+          </li>
+        </ul>
+        <div className='btn-lines'>
+          <div className='btn-cont'>
+            <a href='https://dancing-frangipane-786e1e.netlify.app/'>
+              <button className='btn nav-btn'>Get Started</button>
+            </a>
+          </div>
+          <div
+            className={`lines `}
+            onClick={() => {
+              setNavShow(!navShow);
+            }}
+          >
+            <div
+              className={`line line-1 black ${navShow ? "cross-nav" : null}`}
+            ></div>
+            <div
+              className={`line line-2 black ${navShow ? "rev-cross" : null}`}
+            ></div>
+          </div>
+        </div>
+      </nav>
+      {navShow && (
+        <nav className={`sm-nav ${navShow ? "wh " : null}`} id='sm-nav'>
+          <ul className='sm-nav-items'>
+            <li className='sm-item'>
+              <div className='sm-img' onClick={() => navigate("/our-approach")}>
+                <img src={co2} alt='' />
+              </div>
+              <div
+                className='sm-img-text'
+                onClick={() => navigate("/our-approach")}
+              >
+                Our Approach
+                <p>Understanding the impact of offsets</p>
+              </div>
+            </li>
+            <li className='sm-item'>
+              <div className='sm-img' onClick={() => navigate("/our-projects")}>
+                <img src={forest} alt='' />
+              </div>
+              <div
+                className='sm-img-text'
+                onClick={() => navigate("/our-projects")}
+              >
+                Our Projects
+                <p>See how offset carbon works</p>
+              </div>
+            </li>
+
+            {/* <li className='sm-item'>
+              <div className='sm-img'>
+                <img src={aboutIcon} alt='' />
+              </div>
+              <div className='sm-img-text'>
+                Contact Us
+                <br />
+                Understanding the impact of offsets
+              </div>
+            </li> */}
+
+            <li className='sm-btn-item sm-item'>
+              <button className='btn sm-nav-btn'>Get Started</button>
+            </li>
+          </ul>
+        </nav>
       )}
-    </AppBar>
+    </div>
   );
 };
 
